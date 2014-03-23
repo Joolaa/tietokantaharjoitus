699bc5040a8661efb3319e398101b25034040db5
Jola <jola@Jola.(none)>
2014-03-23 22:08:06 +0200

    sql create table ja drop table

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -1,6 +1,43 @@
+/* TODO: 'on delete' ja muut käyttörajoitteet puuttuvat */
+
+CREATE TABLE kayttaja (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(50),
+    salasana VARCHAR(100),
+    etunimi VARCHAR(20),
+    sukunimi VARCHAR(30)
+);
+
+CREATE TABLE yhteiso (
+    id SERIAL PRIMARY KEY,
+    nimi varchar(30)
+);
+
+CREATE TABLE asiakas (
+    id SERIAL PRIMARY KEY,
+    nimi VARCHAR(30),
+    oletustuntitaksa REAL,
+    yhteiso_id INTEGER REFERENCES yhteiso(id)
+);
+
 CREATE TABLE tyoaikadata (
     id SERIAL PRIMARY KEY,
     alkuaika TIMESTAMP,
     loppuaika TIMESTAMP,
-    aihe VARCHAR(1000)
+    aihe VARCHAR(1000),
+    kayttaja_id INTEGER REFERENCES kayttaja(id),
+    asiakas_id INTEGER REFERENCES asiakas(id),
+    yhteiso_id INTEGER REFERENCES yhteiso(id)
+);
+
+CREATE TABLE yhteiso_kayttaja (
+    kayttaja_id INTEGER REFERENCES kayttaja(id),
+    yhteiso_id INTEGER REFERENCES yhteiso(id),
+    PRIMARY KEY(kayttaja_id, yhteiso_id)
+);
+
+CREATE TABLE yhteison_johtajat (
+    kayttaja_id INTEGER REFERENCES kayttaja(id),
+    yhteiso_id INTEGER REFERENCES yhteiso(id),
+    PRIMARY KEY(kayttaja_id, yhteiso_id)
 );
--- sql/drop-tables.sql
+++ sql/drop-tables.sql
@@ -1,1 +1,6 @@
+DROP TABLE yhteison_johtajat;
+DROP TABLE yhteiso_kayttaja;
 DROP TABLE tyoaikadata;
+DROP TABLE asiakas;
+DROP TABLE yhteiso;
+DROP TABLE kayttaja;
