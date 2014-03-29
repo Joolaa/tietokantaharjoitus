c57c5548c63c6f7a28cc67271fbdc15baea65a01
Jola <jola@Jola.(none)>
2014-03-29 02:27:40 +0200

    korj2

--- views/alerttempl.php
+++ views/alerttempl.php
@@ -1,5 +1,5 @@
 <?php if(!empty($data->error) && $data->error !== ''): ?>
     <div class="alert alert-danger">
-        <?php echo $data->virhe; ?>
+        <?php echo $data->error; ?>
     </div>
 <?php endif; ?>
