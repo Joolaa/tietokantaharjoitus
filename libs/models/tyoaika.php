ab773aed961cb83fe0681417c0a582fb0d7e9347
Jola <jola@Jola.(none)>
2014-04-06 02:39:54 +0300

    uusi yritys

--- libs/models/tyoaika.php
+++ libs/models/tyoaika.php
@@ -63,7 +63,7 @@ class Tyoaika {
 
     public static function searchPagedSortByStartTime($amount,
         $page, $userId) {
-        $sql = "SELECT * FROM tyoaikadata WHERE kayttaja_id = ? LIMIT ? OFFSET ? ORDER BY alkuaika";
+        $sql = "SELECT * FROM tyoaikadata WHERE kayttaja_id = ? LIMIT ? OFFSET ? ORDER by alkuaika";
         $query = getTietokantayhteys()->prepare($sql);
         $query->execute(array($kayttaja_id, $amount,
             ($page-1) * $amount));
