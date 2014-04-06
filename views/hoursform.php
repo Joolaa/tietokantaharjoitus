edaff3905ecc9ec8582585e64a3d6d35f0b5b7b0
Jola <jola@Jola.(none)>
2014-04-06 21:16:44 +0300

    parannus

--- views/hoursform.php
+++ views/hoursform.php
@@ -3,50 +3,50 @@
     <?php require 'alerttempl.php'; ?>
     Alkuaika:<br>
     <div class="row">
-        <div class="form-group" style="width:3em">
+        <div class="form-group" style="max-width:3em">
             <input type="text" class="form-control" 
              placeholder="pp" name="startday">
         </div>/
-        <div class="form-group" style="width:3em">
+        <div class="form-group" style="max-width:3em">
             <input type="text" class="form-control" 
              placeholder="kk" name="startmonth">
         </div>/
-        <div class="form-group" style="width:5em">
+        <div class="form-group" style="max-width:5em">
             <input type="text" class="form-control" 
              placeholder="yyyy" name="startyear">
         </div>
     </div>
     <div class="row">
-        <div class="form-group" style="width:3em">
+        <div class="form-group" style="max-width:3em">
                 <input type="text" class="form-control" 
                 placeholder="tt" name="starthour">
         </div>:
-        <div class="form-group" style="width:3em">
+        <div class="form-group" style="max-width:3em">
             <input type="text" class="form-control" 
              placeholder="mm" name="startminute">
         </div>
     </div>
     Loppuaika:<br>
     <div class="row">
-        <div class="form-group" style="width:3em">
+        <div class="form-group" style="max-width:3em">
             <input type="text" class="form-control" 
              placeholder="pp" name="endday">
         </div>/
-        <div class="form-group" style="width:3em">
+        <div class="form-group" style="max-width:3em">
             <input type="text" class="form-control" 
              placeholder="kk" name="endmonth">
         </div>/
-        <div class="form-group" style="width:5em">
+        <div class="form-group" style="max-width:5em">
             <input type="text" class="form-control" 
              placeholder="yyyy" name="endyear">
         </div>
     </div>
     <div class="row">
-        <div class="form-group" style="width:3em">
+        <div class="form-group" style="max-width:3em">
                 <input type="text" class="form-control" 
                 placeholder="tt" name="endhour">
         </div>:
-        <div class="form-group" style="width:3em">
+        <div class="form-group" style="max-width:3em">
             <input type="text" class="form-control" 
              placeholder="mm" name="endminute">
         </div>
