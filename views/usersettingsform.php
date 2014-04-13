52b05302aed09910d84b4e3957bdb099e002154a
Jola <jola@Jola.(none)>
2014-04-13 21:20:22 +0300

    asetukset

--- views/usersettingsform.php
+++ views/usersettingsform.php
@@ -1,1 +1,43 @@
-<p> form here </p>
+<h1>Asetusten muuttaminen</h1>
+<br><br><br>
+<p>Sähköpostiosoitteen vaihtaminen</p>
+<form action="usersettings.php?email" method="POST"
+    class="form-inline" role="form">
+    <div class="row-fluid">
+        <div class="form-group">
+            <input type="text" class="form-control"
+             placeholder="Uusi sähköposti" name="email">
+        </div>
+        <button type="submit" class="btn btn-primary">
+            Vaihda
+        </button>
+    </div>
+</form>
+<br><br><br>
+<p>Etunimen vaihtaminen</p>
+<form action="usersettings.php?firstname" method="POST"
+    class="form-inline" role="form">
+    <div class="row-fluid">
+        <div class="form-group">
+            <input type="text" class="form-control"
+             placeholder="Uusi etunimi" name="firstname">
+        </div>
+        <button type="submit" class="btn btn-primary">
+            Vaihda
+        </button>
+    </div>
+</form>
+<br><br><br>
+<p>Sukunimen vaihtaminen</p>
+<form action="usersettings.php?lastname" method="POST"
+    class="form-inline" role="form">
+    <div class="row-fluid">
+        <div class="form-group">
+            <input type="text" class="form-control"
+             placeholder="Uusi sähköposti" name="lastname">
+        </div>
+        <button type="submit" class="btn btn-primary">
+            Vaihda
+        </button>
+    </div>
+</form>
