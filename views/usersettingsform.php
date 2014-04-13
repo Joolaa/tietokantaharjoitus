239b847b5b6f5ce2a730ed5bbf14ed972cbca2c7
Jola <jola@Jola.(none)>
2014-04-13 21:39:21 +0300

    asetusnäkymä valmis

--- views/usersettingsform.php
+++ views/usersettingsform.php
@@ -1,5 +1,7 @@
 <h1>Asetusten muuttaminen</h1>
-<br><br><br>
+<br><br>
+<?php require 'alerttempl.php'; ?>
+<br>
 <p>Sähköpostiosoitteen vaihtaminen</p>
 <form action="usersettings.php?email" method="POST"
     class="form-inline" role="form">
@@ -13,7 +15,7 @@
         </button>
     </div>
 </form>
-<br><br><br>
+<br><br>
 <p>Etunimen vaihtaminen</p>
 <form action="usersettings.php?firstname" method="POST"
     class="form-inline" role="form">
@@ -27,17 +29,61 @@
         </button>
     </div>
 </form>
-<br><br><br>
+<br><br>
 <p>Sukunimen vaihtaminen</p>
 <form action="usersettings.php?lastname" method="POST"
     class="form-inline" role="form">
     <div class="row-fluid">
         <div class="form-group">
             <input type="text" class="form-control"
-             placeholder="Uusi sähköposti" name="lastname">
+             placeholder="Uusi sukunimi" name="lastname">
         </div>
         <button type="submit" class="btn btn-primary">
             Vaihda
+        </button>
+    </div>
+</form>
+<br><br>
+<p>Salasanan vaihtaminen</p>
+<form action="usersettings.php?password" method="POST"
+    role="form">
+    <div class="row-fluid">
+        <div class="form-group">
+            <input type="password" class=form-control"
+             placeholder="Vanha salasana" name="oldpass">
+        </div>
+    </div>
+    <div class="row-fluid">
+        <div class="form-group">
+            <input type="password" class=form-control"
+             placeholder="Uusi salasana" name="newpass">
+        </div>
+    </div>
+    <div class="row-fluid">
+        <div class="form-group">
+            <input type="password" class=form-control"
+             placeholder="Uusi salasana uudestaan"
+             name="newpassconfirm">
+        </div>
+    </div>
+    <div class="row-fluid">
+        <button type="submit" class="btn btn-primary">
+            Vaihda
+        </button>
+    </div>
+</form>
+<br><br>
+<p>Tilin poistaminen</p>
+<p>Syötä salasanasi varmistaaksesi tilin poistaminen</p>
+<form action="usersettings.php?delete" method="POST"
+    role="form">
+    <div class="row-fluid">
+        <div class="form-group">
+            <input type="password" class="form-control"
+             placeholder="Salasana" name="password">
+        </div>
+        <button type=submit" class="btn btn-danger">
+            Poista tili
         </button>
     </div>
 </form>
