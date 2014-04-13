3f635335b86b0448e8b966da2f8b1ecdd8faeea3
Jola <jola@Jola.(none)>
2014-04-13 18:19:25 +0300

    korjaus

--- views/nav.php
+++ views/nav.php
@@ -35,7 +35,7 @@ function makepillactive_pohja($currentpage, $thispage) {
     <p class="nav navbar-text">Kirjautuneena: <?php $data->user->echoUserFullName(); ?></p>
     <ul class="nav nav-pills navbar-left">
         <li>
-            <a href="#">
+            <a href="usersettings.php">
                 Asetukset
             </a>
         </li>
