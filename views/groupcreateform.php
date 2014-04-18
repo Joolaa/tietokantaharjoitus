ff7d24c8dc7afce62e40ceb8d6126a93f958a11c
Jola <jola@Jola.(none)>
2014-04-18 17:20:52 +0300

    ryhmän nimeämislomake

--- views/groupcreateform.php
+++ views/groupcreateform.php
@@ -2,4 +2,16 @@
 <?php 
 require 'alerttempl.php'; 
 require 'noticetempl.php';?>
-<p>Anna ryhmällesi nimi:</p>
+<p>Anna ryhmällesi nimi:</p><br>
+<form action="groupcreation.php" method="POST"
+    class="form-inline" role="form">
+    <div class="row-fluid">
+        <input type="text" class="form-control"
+            placeholder="Ryhmän nimi" name="grpname">
+    </div>
+    <div class="row">
+        <button type="submit" class="btn btn-primary">
+            Hyväksy
+        </button>
+    </div>
+</form>
