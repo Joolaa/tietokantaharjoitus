0823fc3b7a2403d0e89dd26a3d89569e0677f17c
Jola <jola@Jola.(none)>
2014-04-24 01:50:55 +0300

    tyhmän virheen korjaus

--- views/groupmanageview.php
+++ views/groupmanageview.php
@@ -2,7 +2,7 @@
 <div class="rightelem">
     <p>Kutsu käyttäjä ryhmään:</p>
     <form class="form-inline" method="POST"
-     action="groupmanagement.php?grpid="<?php echo $grpid;?>>
+     action="groupmanagement.php?grpid="<?php echo $data->grpid;?>>
         <div class="row-fluid">
         <input type="text" class="form-control" name="invite"
          placeholder="Käyttäjän sähköposti">
