c136be7aa1378dd6d91e2517b30d3f1ccc4a6e23
Jola <jola@Jola.(none)>
2014-04-24 03:08:09 +0300

    tiedosto lisätty

---
+++ invitehandler.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'libs/common.php';
+require_once 'libs/models/kayttaja.php';
+require_once 'libs/models/yhteiso';
+
+$userobj = isLoggedDirectToLogin();
+$grpid = null;
+
+if(isset($_GET['cancel'])) {
+    $grpid = $_GET['cancel'];
+} elseif(isset($_GET['join'])) {
+    $grpid = $_GET['join']
+    Yhteiso::insertNewMember($userobj->getId(), $grpid);
+}
+
+if(!is_null($grpid)) {
+    Yhteiso::removeInvitation($userobj->getId(), $grpid);
+}
+
+header('Location: profile.php');
