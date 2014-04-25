9fd2b7810303476463b83e755e9ea37f076c6c27
Jola <jola@Jola.(none)>
2014-04-25 11:24:02 +0300

    etusivu

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'common.php';
+
+showView('frontpage.php',
+    'title' => 'Työaikatietokanta'
+));
---
+++ views/frontpage.php
@@ -0,0 +1,4 @@
+<h1>Tervetuloa työaikatietokantaan</h1>
+<p>Jos sinulla on käyttäjätunnus, kirjaudu sisään.</p>
+<p>Jos sinulla ei ole vielä käyttäjätunnusta, voit luoda
+sellaisen rekisteröitymällä.</p>
